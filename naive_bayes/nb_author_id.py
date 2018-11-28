e70ea88b9375ad2ee67922552c47a6471acbe6b6
liujinzhou <liujinzhou@jd.com>
2018-11-28 20:12:21 +0800

    gnb case

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,21 +12,22 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
+from tools.email_preprocess import preprocess
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(features_train,labels_train)
+predict = gnb.predict(features_test)
+print(11111111111111)
+print(predict)
 
 #########################################################
 
