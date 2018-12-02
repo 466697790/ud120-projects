b5e4d65078e24bc6d6f3a63f0663daf7661315c1
liujinzhou <liujinzhou@jd.com>
2018-12-02 23:25:34 +0800

    enron_data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,16 +18,43 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
-print(type(enron_data),len(enron_data))
+print(type(enron_data), len(enron_data))
 f_count = len(enron_data['METTS MARK'])
 print(f_count)
 enron_data_list = list(enron_data)
 print(enron_data_list)
 print(len(enron_data_list))
 
+# count = 0
+# for p in enron_data_list:
+#     if enron_data[p]['poi'] == 1:
+#         count += 1
+
+# print(count)
+
+# stock = enron_data['PRENTICE JAMES']['total_stock_value']
+# print(stock)
+# email_count = enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+# print(email_count)
+# value = enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+# print(value)
+
+print(enron_data['METTS MARK'])
 count = 0
 for p in enron_data_list:
-    if enron_data[p]['poi'] == 1:
+    # if enron_data[p]['salary'] != 'NaN':
+    if enron_data[p]['email_address'] != 'NaN':
         count += 1
 
 print(count)
+
+
+
+
+
+
+
+
+
+
+
